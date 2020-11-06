97cd937c86ce218a01ad8012f5998bf2e6664092
Qalac <officialqalac@gmail.com>
2020-11-06 13:17:39 +0000

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
+import { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-        <p>
-          The edit is without issue
-        </p>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedFile: null
+    }
+    this.onChangeHandler = this.onChangeHandler.bind(this);
+  }
+
+  onChangeHandler = e => {
+    console.log(e.target.files[0])
+    this.setState({
+      selectedFile: e.target.files[0],
+      loaded: 0
+    })
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="form-group files">
+            <label>Upload files here</label>
+            <input className="form-control" onChange={this.onChangeHandler} type="file" name="file" 
+              accept=".csv, .xlsx, .xls" ></input>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 export default App;
