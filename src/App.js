bcd0818f14a698f040b50dc389aefa24f7c2a512
Qalac <officialqalac@gmail.com>
2020-11-07 00:11:51 +0000

    temporary design

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import { Component } from 'react';
 import axios from 'axios';
 import './App.css';
-import { ProSidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
-import 'react-pro-sidebar/dist/css/styles.css';
 
 class App extends Component {
   constructor(props) {
@@ -33,20 +31,29 @@ class App extends Component {
 
   render() {
     return (
-      <ProSidebar>
-        <Menu iconShape="square">
-          <MenuItem icon={<FaGem />}>Dashboard</MenuItem>
-          <SubMenu title="Components" icon={<FaHeart />}>
-            <MenuItem>Component 1</MenuItem>
-            <MenuItem>Component 2</MenuItem>
-          </SubMenu>
-        </Menu>
-      </ProSidebar>
+      <div className="input-group">
+        <div className="input-group-prepend">
+          <span className="input-group-text" id="inputGroupFileAddon01">
+            Upload
+          </span>
+        </div>
+        <div className="custom-file">
+          <input
+            type="file"
+            onChange={this.onChangeHandler}
+            className="custom-file-input"
+            id="inputGroupFile01"
+            aria-describedby="inputGroupFileAddon01"
+            accept=".csv"
+          />
+          <label className="custom-file-label" htmlFor="inputGroupFile01">
+            Choose file
+          </label>
+        </div>
+      </div>
     );
   }
 }
-
-
 
 
 {/* <div className="file-form">
