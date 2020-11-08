be99901b0124dad2368774e4408e0f6a7753a05c
Qalac <officialqalac@gmail.com>
2020-11-08 02:33:23 +0000

    dd

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { Component } from 'react';
 import axios from 'axios';
 import './App.css';
+import {Button} from 'react-bootstrap'
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       selectedFile: null,
-      fileName: 'Please select file'
+      fileName: 'Please choose file',
+      message: 'nnn'
     }
     this.onChangeHandler = this.onChangeHandler.bind(this);
     this.onClickHandler = this.onClickHandler.bind(this);
@@ -16,15 +18,18 @@ class App extends Component {
   onChangeHandler = e => {
     this.setState({
       selectedFile: e.target.files[0],
+      fileName: e.target.files[0].name
     })
   }
 
   onClickHandler = () => {
     const data = new FormData()
     data.append('file', this.state.selectedFile)
-     axios.post("http://127.0.0.1:5000/upload", data)
+     axios.post("http://45.32.16.104/upload", data)
        .then(res => {
-         console.log(res.status)
+         this.setState({
+           message: res.data.response
+         })
        })
        .catch(err => console.log(err))
   }
@@ -32,25 +37,29 @@ class App extends Component {
 
   render() {
     return (
-      <div className="input-group">
-        <div className="input-group-prepend">
-          <span className="input-group-text" id="inputGroupFileAddon01">
-            Upload
-          </span>
-        </div>
-        <div className="custom-file">
-          <input
-            type="file"
-            onChange={this.onChangeHandler}
-            className="custom-file-input"
-            id="inputGroupFile01"
-            aria-describedby="inputGroupFileAddon01"
-            accept=".csv"
-          />
-          <label className="custom-file-label" htmlFor="inputGroupFile01">
-            {this.state.fileName}
-          </label>
-        </div>
+      <div>
+        <div className="input-group">
+          <div className="input-group-prepend">
+            <span className="input-group-text" id="inputGroupFileAddon01">
+              Upload
+            </span>
+          </div>
+          <div className="custom-file">
+            <input
+              type="file"
+              onChange={this.onChangeHandler}
+              className="custom-file-input"
+              id="inputGroupFile01"
+              aria-describedby="inputGroupFileAddon01"
+              accept=".csv"
+            />
+            <label className="custom-file-label" htmlFor="inputGroupFile01">
+              {this.state.fileName}
+            </label>
+          </div>
+        </div><br />
+        <Button onClick={this.onClickHandler}>Upload</Button>
+        {this.state.message}
       </div>
     );
   }
